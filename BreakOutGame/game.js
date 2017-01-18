0c4a4baed7ef091a6a3a1593d5348af5fa7c6721
gitpash <arklogin@gmail.com>
2017-01-18 20:05:18 +0300

    draw bricks

--- BreakOutGame/game.js
+++ BreakOutGame/game.js
@@ -29,8 +29,28 @@ let paddleWidth = 75
 let paddleX = (canvas.width-paddleWidth)/2
 let rightPressed = false
 let leftPressed = false
+let brickRowCount = 3
+let brickColumnCount = 5
+let brickWidth = 75
+let brickHeight = 20
+let brickPadding = 10
+let brickOffsetTop = 30
+let brickOffsetLeft = 30
+
+let bricks = [] // двумерный массив блоков
+for (c = 0; c < brickColumnCount; c++) { // содержит колонны
+  bricks[c] = []
+  for (r = 0; r < brickRowCount; r++) { // колонны содержат ряды
+    bricks[c][r] = { x: 0, // ряд содердит объект с координатами
+                     y: 0 }
+  }
+}
 document.addEventListener("keydown", keyDownHandler, false)
 document.addEventListener("keyup", KeyUpHandler, false)
+
+
+
+
 //добавляем реакцию на событие
 
 // когда кнопка нажата - значение true записывается в переменную
@@ -55,9 +75,9 @@ function KeyUpHandler(e) {
 // рисуем мяч
 function drawBall() {
   ctx.beginPath()
-  ctx.arc(x, y, ballRadius, 0, Math.PI*2, false) //1 и 2 - координаты центра, 3я-радиус, 4и5 начальный/конечный угол отрисовки
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2) //1 и 2 - координаты центра, 3я-радиус, 4и5 начальный/конечный угол отрисовки
                                             // в радианах, направление отрисовки(false по часовой и true против) опционально
-  ctx.fillStyle = "#0095FF"
+  ctx.fillStyle = "#0095DD"
   ctx.fill()
   ctx.closePath()
 }
@@ -70,19 +90,56 @@ function drawPaddle() {
   ctx.closePath()
 }
 
+//  рисуем блоки, проходим по кол и рядам и задаем координаты блоков
+function drawBricks() {
+  for (c = 0; c < brickColumnCount; c++) {
+    for (r = 0; r < brickRowCount; r++) {
+      // определяем коорд. блока по Х это его ширина+отступ между блоками помн. номер колонны + отступ от слева
+      let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft
+      // коорд. по У это высота+отступ между помн. на номер строки + отступ сверху
+      let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop
+
+      bricks[c][r].x = brickX
+      bricks[c][r].y = brickY
+      ctx.beginPath()// отрисовываем полученые блоки канвасом
+      ctx.rect(brickX, brickY, brickWidth, brickHeight)
+      ctx.fillStyle = "#0095DD"
+      ctx.fill()
+      ctx.closePath()
+
+    }
+  }
+}
 // отрисовка игрового поля
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height) //этот метод очищает поле канваса с принимая аргуметами
-                                                  //1 и 2 коорд. левого верхнего угла и 3 и 4 коорд. правого нижнего
-  drawBall() // вызываем мяч
-  drawPaddle()
+  drawBricks() // рисуем кирпичи                                               //1 и 2 коорд. левого верхнего угла и 3 и 4 коорд. правого нижнего
+  drawBall() // рисуем мяч
+  drawPaddle() // рисуем площадку
+
+  //условие отскока от левого и правого краев
   if(x + dx > (canvas.width - ballRadius) || x + dx < ballRadius) {
     dx = -dx
   }
 
-  if(y + dy > (canvas.height - ballRadius) || y + dy < ballRadius) {
+  // отскок от потолка
+  if(y + dy < ballRadius) {
     dy = -dy
   }
+
+  // если центр мяча касается площадки - отскок
+  else if(y + dy > (canvas.height - ballRadius)) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      if(y = y - paddleHeight) {
+      dy = -dy
+  }
+    }
+    // если мяч касается нижнего края - выводим алерт
+    else {
+    alert('Ooops! Something went wrong...')
+    document.location.reload()// перезагружаем страницу
+  }
+}
   // движение площадки в рамках окна канваса
   if(rightPressed && paddleX < canvas.width - paddleWidth) {
     paddleX += 7
