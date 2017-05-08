33c5012e97ba4693cca3031bec2676d45247d666
gitpash <arklogin@gmail.com>
2017-05-08 23:37:42 +0300

    continue

--- Async_Await_by_Egghead/lesson_1.js
+++ Async_Await_by_Egghead/lesson_1.js
@@ -9,6 +9,7 @@ function showGitUser(handle) {
 }
 showGitUser("gitpash");
 
+// do the same thing with async/await
 async function showGithubUser(handle) {
   const url = `https://api.github.com/users/${handle}`;
   const response = await fetch(url);
@@ -16,4 +17,20 @@ async function showGithubUser(handle) {
   console.log(user.name);
   console.log(user.location);
 }
-showGithubUser("gitpash");
+// showGithubUser("gitpash");
+
+// do the func just resolve Promise and check this with chaining after that
+async function showGithubUser(handle) {
+  const url = `https://api.github.com/users/${handle}`;
+  const response = await fetch(url);
+  const user = await response.json();
+  /***/ return user;
+  
+  // or we can just refactor a bit
+  //return await response.json()
+}
+
+showGithubUser("gitpash").then(user => {
+  console.log(user.name);
+  console.log(user.location);
+});
